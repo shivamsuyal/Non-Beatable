6254c00843a8b51a16201113da1faf18c4d441a8
shivamsuyal <75232486+shivamsuyal@users.noreply.github.com>
2021-01-24 12:59:08 +0530

    Add files via upload

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -47,6 +47,7 @@ def printBoard(board):
 
 def ai1():
   global move,ai1_move
+  ai1_move=0
   if ((d['n2']+d['n3']==8) | (d['n5']+d['n9']==8) | (d['n4']+d['n7']==8)) and (1 in lis):
     move=1
     ai1_move=1
@@ -74,45 +75,50 @@ def ai1():
   elif ((d['n5']+d['n1']==8) | (d['n8']+d['n7']==8) | (d['n3']+d['n6']==2)) and (9 in lis):
     move=9
     ai1_move=1
-  else:
-    l=random.choice(range(len(lis)-1))
-    move=lis[l]
-    ai1_move=1
+    
 
 
 def ai2 ():
   global move
+  
   if ((d['n2']+d['n3']==2) | (d['n5']+d['n9']==2) | (d['n4']+d['n7']==2)) and (1 in lis):
     move=1
+    
   
   elif ((d['n3']+d['n1']==2) | (d['n5']+d['n8']==2)) and (2 in lis):
     move=2
+    
   
   elif ((d['n2']+d['n1']==2) | (d['n5']+d['n7']==2) | (d['n9']+d['n6']==2)) and (3 in lis):
     move=3
+    
   
   elif ((d['n7']+d['n1']==2) | (d['n5']+d['n6']==2)) and (4 in lis):
     move=4
     
+  
   elif ((d['n2']+d['n8']==2) | (d['n4']+d['n6']==2) | (d['n9']+d['n1']==2) | (d['n7']+d['n3']==2)) and (5 in lis):
     move=5  
     
+  
   elif ((d['n5']+d['n4']==2) | (d['n9']+d['n3']==2)) and (6 in lis):
     move=6
     
+  
   elif ((d['n4']+d['n1']==2) | (d['n5']+d['n3']==2) | (d['n9']+d['n8']==2)) and (7 in lis):
     move=7
     
+  
   elif ((d['n2']+d['n5']==2) | (d['n9']+d['n7']==2)) and (8 in lis):
     move=8
     
+  
   elif ((d['n5']+d['n1']==2) | (d['n8']+d['n7']==2) | (d['n3']+d['n6']==2)) and (9 in lis):
     move=9
     
   else:
     l=random.choice(range(len(lis)-1))
     move=lis[l]
-
 
 def win():
   k1= d['n1']+d['n2']+d['n3']
@@ -168,7 +174,6 @@ def play():
             ai1()
             if ai1_move==0:
                 ai2()
-            ai1_move=0
             print('Computer marked ')
             theBoard[str(move)] = 'O'
             d['n'+str(move)] = 4
@@ -231,3 +236,7 @@ while True:
    play()
  except draw:
    print('No one won')
+   break
+ except:
+  print('Game Over')
+  break
